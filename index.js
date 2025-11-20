b0ae42b656f52027053785dddb76489a3807be5e
muslimsislomjonova-cmd <muslimsislomjonova@gmail.com>
2025-11-20 22:27:57 +0500

    fizz

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const speeds = {
-    walk: 3.6,
-    bike: 20.1,
-    car: 70,
-    plane: 800
-};
+let speedWalk = 3.6;
+let speedBike = 20.1;
+let speedCar = 70;
+let speedPlane = 800;
 
 
 function convertTime(hours) {
     let h = Math.floor(hours);
     let m = Math.round((hours - h) * 60);
-    return `${h} soat ${m} daqiqa`;
+    return h + " soat " + m + " daqiqa";
 }
 
 document.getElementById("calcBtn").addEventListener("click", function () {
@@ -20,9 +18,14 @@ document.getElementById("calcBtn").addEventListener("click", function () {
         return;
     }
 
-   
-    document.getElementById("walk").innerText = convertTime(distance / speeds.walk);
-    document.getElementById("bike").innerText = convertTime(distance / speeds.bike);
-    document.getElementById("car").innerText = convertTime(distance / speeds.car);
-    document.getElementById("plane").innerText = convertTime(distance / speeds.plane);
+    let walkTime = distance / speedWalk;
+    let bikeTime = distance / speedBike;
+    let carTime = distance / speedCar;
+    let planeTime = distance / speedPlane;
+
+  
+    document.getElementById("walk").innerText = convertTime(walkTime);
+    document.getElementById("bike").innerText = convertTime(bikeTime);
+    document.getElementById("car").innerText = convertTime(carTime);
+    document.getElementById("plane").innerText = convertTime(planeTime);
 });
